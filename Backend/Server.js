ac8dfc414b62c26d0deb229083e7a89679a31861
anasbughio <anasbughio@gmail.com>
2025-08-24 01:47:16 +0500

    Update Server.js with Vercel CORS origin

--- Backend/Server.js
+++ Backend/Server.js
@@ -7,26 +7,16 @@ const authRoutes = require('./routes/authRoutes');
 const authMiddleware = require("./middlewares/authMiddlewares");
 
 const app = express();
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://notes-app-olive-phi.vercel.app",
-  "https://notes-app-git-main-anas-projects-ddcc6eed.vercel.app"
-];
 
 dotenv.config();
 const PORT = process.env.PORT;
 app.use(express.json());
-app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin (like mobile apps, curl, etc.)
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS: " + origin));
-    }
-  },
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "https://notes-app-olive-phi.vercel.app",
+    credentials: true,
+  })
+);
 app.use('/api/notes',authMiddleware,notesRoutes);
 app.use('/api/auth',authRoutes);
 
