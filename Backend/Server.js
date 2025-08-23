321783c4926373e4f2e0b9157817f1fdc90f8999
anasbughio <anasbughio@gmail.com>
2025-08-23 23:51:02 +0500

    Fix package.json and Server.js config

--- Backend/Server.js
+++ Backend/Server.js
@@ -23,6 +23,7 @@ mongoose.connect(process.env.MONGO_URI)
     app.listen(PORT,()=>{
         console.log(`Server is running on port ${PORT}`);
     })
+    
 })
 .catch((err)=>{
     console.error(`Error Connecting to MongoDB:${err.message}`);
