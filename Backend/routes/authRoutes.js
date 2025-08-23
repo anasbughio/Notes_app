c216ef5eac1092602a900168211f010dd15a4263
anasbughio <anasbughio@gmail.com>
2025-08-24 00:08:15 +0500

    Fix: corrected authControllers import path

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/authCOntrollers');
+const { registerUser, loginUser } = require('../controllers/authControllers');
 
 
 router.post('/register',registerUser);
