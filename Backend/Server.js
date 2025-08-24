42b233ef52b910ae12b5986ae5d9eb6ee1c93300
anasbughio <anasbughio@gmail.com>
2025-08-25 02:14:46 +0500

    Fix preflight OPTIONS for Vercel frontend

--- Backend/Server.js
+++ Backend/Server.js
@@ -2,8 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const notesRoutes =;
-const authRoutes = ;
+
 
 dotenv.config();
 const app = express();
