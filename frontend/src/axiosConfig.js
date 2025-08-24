9d488dc8c7c1f6adafb95b650f30110365eae222
anasbughio <anasbughio@gmail.com>
2025-08-25 01:41:14 +0500

    Update axiosConfig to use correct REACT_APP_API_URL

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
-
+console.log(process.env.REACT_APP_API_URL);
 const instance = axios.create({
-    baseURL: "https://notesapp-production-173d.up.railway.app/api"
+    baseURL:process.env.REACT_APP_API_URL,
+        
 
 
 });
